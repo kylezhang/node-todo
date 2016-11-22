5714d5e3ca2c075832f7265672c87f1c491badae
zhangkai <zkxw2008@hotmial.com>
2016-11-22 15:43:26 +0800

    added field:is_deleted, created_at, updated_at, and logical

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,8 +1,21 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 module.exports = mongoose.model('Todo', {
     text: {
         type: String,
         default: ''
+    },
+    is_deleted: {
+        type: Number,
+        default: 1   //is deleted, 0: undeleted, 1: deleted
+    },
+    created_at: {
+        type: String,
+        default: moment().format('YYYY-MM-DD HH:MM:SS')
+    },
+    updated_at: {
+        type: String,
+        default: moment().format('YYYY-MM-DD HH:MM:SS')
     }
 });
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 var Todo = require('./models/todo');
+var moment = require('moment');
 
 function getTodos(res) {
-    Todo.find(function (err, todos) {
+    Todo.find({ is_deleted : 0 }, function (err, todos) {
 
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
         if (err) {
@@ -27,6 +28,7 @@ module.exports = function (app) {
         // create a todo, information comes from AJAX request from Angular
         Todo.create({
             text: req.body.text,
+            is_deleted: 0,
             done: false
         }, function (err, todo) {
             if (err)
@@ -40,9 +42,12 @@ module.exports = function (app) {
 
     // delete a todo
     app.delete('/api/todos/:todo_id', function (req, res) {
-        Todo.remove({
-            _id: req.params.todo_id
-        }, function (err, todo) {
+        var update = { _id : req.params.todo_id }
+        var opts   = {
+            'is_deleted' : 1,
+            'updated_at' : moment().format('YYYY-MM-DD HH:MM:SS')
+        }
+        Todo.update(update, opts, function (err) {
             if (err)
                 res.send(err);
 
